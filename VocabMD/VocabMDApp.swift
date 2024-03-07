45ef526d471b7b7741299aa385a13e403456bdaa
Henry Freck <henry.freck@gmail.com>
2024-03-07 13:39:03 -0800

    Initial Commit

---
+++ VocabMD/VocabMDApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabMDApp.swift
+//  VocabMD
+//
+//  Created by Henry Freck on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct VocabMDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
